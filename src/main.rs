50d97a54df1c7755d4bbaffa3059cb72215f4f8e
John Hall <johngradyh@gmail.com>
2023-07-07 15:20:43 -0400

    parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,68 @@
+mod test;
+
 use nix::pty::forkpty;
 use nix::unistd::{read, ForkResult};
 use std::os::fd::RawFd;
 use std::process::Command;
+use vte::{Params, Parser, Perform};
+
+struct Performer;
+
+impl Perform for Performer {
+    fn print(&mut self, c: char) {
+        println!("[print] {:?}", c);
+    }
+
+    fn execute(&mut self, byte: u8) {
+        println!("[execute] {:02x}", byte);
+    }
+
+    fn hook(&mut self, params: &Params, intermediates: &[u8], ignore: bool, c: char) {
+        println!(
+            "[hook] params={:?}, intermediates={:?}, ignore={:?}, char={:?}",
+            params, intermediates, ignore, c
+        );
+    }
+
+    fn put(&mut self, byte: u8) {
+        println!("[put] {:02x}", byte);
+    }
+
+    fn unhook(&mut self) {
+        println!("[unhook]");
+    }
+
+    fn osc_dispatch(&mut self, params: &[&[u8]], bell_terminated: bool) {
+        println!(
+            "[osc_dispatch] params={:?} bell_terminated={}",
+            params, bell_terminated
+        );
+    }
+
+    fn csi_dispatch(&mut self, params: &Params, intermediates: &[u8], ignore: bool, c: char) {
+        println!(
+            "[csi_dispatch] params={:#?}, intermediates={:?}, ignore={:?}, char={:?}",
+            params, intermediates, ignore, c
+        );
+    }
+
+    fn esc_dispatch(&mut self, intermediates: &[u8], ignore: bool, byte: u8) {
+        println!(
+            "[esc_dispatch] intermediates={:?}, ignore={:?}, byte={:02x}",
+            intermediates, ignore, byte
+        );
+    }
+}
+
+fn parse_ansi_escape(read_bytes: Vec<u8>) -> () {
+    let mut parser = Parser::new();
+
+    let mut log = Performer;
+
+    for byte in &read_bytes {
+        parser.advance(&mut log, *byte)
+    };
+}
 
 fn read_from_fd(fd: RawFd) -> Option<Vec<u8>> {
     let mut read_buffer = [0; 65536];
@@ -22,6 +83,8 @@ fn spawn_pty_with_shell(default_shell: String) -> RawFd {
                         .spawn()
                         .expect("failed to spawn");
                     std::thread::sleep(std::time::Duration::from_millis(2000));
+                    Command::new("ls").spawn().expect("failed ls");
+                    std::thread::sleep(std::time::Duration::from_millis(2000));
                     std::process::exit(0);
                 }
                 stdout_fd
@@ -40,7 +103,8 @@ fn main() {
     loop {
         match read_from_fd(stdout_fd) {
             Some(mut read_bytes) => {
-                read_buffer.append(&mut read_bytes);
+                // read_buffer.append(&mut read_bytes);
+                parse_ansi_escape(read_bytes); 
             }
             None => {
                 println!("{:?}", String::from_utf8(read_buffer).unwrap());
