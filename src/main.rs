77c1f5a706936607b9aeed1a33b8d6114ee86f7f
John Hall <johngradyh@gmail.com>
2023-07-07 14:30:00 -0400

    spawning, reading

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
+use nix::pty::forkpty;
+use nix::unistd::{read, ForkResult};
 use std::os::fd::RawFd;
+use std::process::Command;
 
 fn read_from_fd(fd: RawFd) -> Option<Vec<u8>> {
-    unimplemented!()
+    let mut read_buffer = [0; 65536];
+    let read_result = read(fd, &mut read_buffer);
+    match read_result {
+        Ok(bytes_read) => Some(read_buffer[..bytes_read].to_vec()),
+        Err(_e) => None,
+    }
 }
 
 fn spawn_pty_with_shell(default_shell: String) -> RawFd {
-    unimplemented!()
+    unsafe {
+        match forkpty(None, None) {
+            Ok(fork_pty_res) => {
+                let stdout_fd = fork_pty_res.master;
+                if let ForkResult::Child = fork_pty_res.fork_result {
+                    Command::new(&default_shell)
+                        .spawn()
+                        .expect("failed to spawn");
+                    std::thread::sleep(std::time::Duration::from_millis(2000));
+                    std::process::exit(0);
+                }
+                stdout_fd
+            }
+            Err(e) => {
+                panic!("forkpty failed: {}", e);
+            }
+        }
+    }
 }
 
 fn main() {
